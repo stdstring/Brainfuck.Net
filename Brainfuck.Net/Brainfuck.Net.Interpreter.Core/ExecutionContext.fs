0f6e559d5265055d0e5f3f7519ded5267472f7b9
Ushakov Andrey <std_string@mail.ru>
2016-09-29 14:36:53 +0500

    add lib and app projects for interpretor

---
+++ Brainfuck.Net/Brainfuck.Net.Interpreter.Core/ExecutionContext.fs
@@ -0,0 +1,15 @@
+﻿namespace Brainfuck.Net.Interpreter.Core
+
+open System
+open System.Collections.Generic
+open System.IO
+
+type ExecutionContext =
+    {
+        mutable OpCount : int32;
+        mutable Ip : int32;
+        mutable CurrentCell : int32
+        Stack : Stack<Int32>;
+        Input : Stream
+        Output : Stream
+    }
---
+++ Brainfuck.Net/Brainfuck.Net.Interpreter.Core/Interpreter.fs
@@ -0,0 +1,2 @@
+﻿namespace Brainfuck.Net.Interpreter.Core
+
